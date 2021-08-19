08567badf17811e3167e7b88e152894c1183cd6b
mazed <mazedur@gmail.com>
2021-08-19 23:48:11 +0600

    problems sets added upto 10

---
+++ problems_1-25.py
@@ -0,0 +1,54 @@
+# Question-001 : Let's start with easy things first. What will the following code produce?
+a = 2
+a = 4
+a = 6
+print(a + a + a)
+
+# Question-002 : What's wrong with the following script?
+a   = 1
+_a  = 2
+_a2 = 3
+2a  = 4
+
+# Question-003 : Executing the code will throw an error. Can you explain why?
+a = 1
+b = 2
+print(a == b)
+print(b == c)
+
+# Question-004 : The last line so that it outputs the sum of 1 and 2. Please do not change the first two lines. Only the last one.
+a = "1"
+b = 2
+print(a + b)
+
+Expected output: 3 
+
+# Question-005 : Complete the script so that it prints out the second item of the list.
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+Expected output: b
+
+# Question-006 : Please complete the script so that it prints out a list slice containing items d , e , and f .
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+Expected output: ['d', 'e', 'f'] 
+
+# Question-007 : Please complete the script so that it prints out the first three items of list letters.		
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+Expected output: ['a', 'b', 'c'] 
+
+# Question-008 : Complete the script so that it prints out letter i using negative indexing.
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+Expected output: i 
+
+# Question-009 : Complete the script so that it prints out a list slice containing the last three items of list letters .
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+Expected output: ['h', 'i', 'j'] 
+
+# Question: Complete the script so that it prints out a list slice containing letters a, c, e, g, and i. 
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+Expected output: ['a', 'c', 'e', 'g', 'i'] 
