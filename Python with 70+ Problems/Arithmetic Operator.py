894454964c9ea7c4926b5389ce7be48c39af3e0e
mazed <mazedur@gmail.com>
2021-08-20 20:35:03 +0600

    Arithmetic Operator

---
+++ Python with 70+ Problems/Arithmetic Operator.py
@@ -0,0 +1,13 @@
+# Do the following arithmetic operations in computer
+
+1.Add 5 and 6 
+
+2.Subtract 16 and 5 
+
+3.Multiply 5 by 3 
+
+4.Divide 13 by 2 
+
+5.Floor division 13 by 2 
+
+6.Modulus of 19 by 7
--- problems_26-50.py
+++ problems_26-50.py
@@ -4,8 +4,8 @@
 
 print(type("Hey".replace("ey","i")[-1])
 
-# Question-028 : The code is supposed to ask the user to enter their name and surname and then it prints out those user submitted values. Instead, the code throws a TypeError. 
-# 		    Please fix it so that the expected output is printed out.
+# Question-028 : The code is supposed to ask the user to enter their name and surname and then it prints out those user submitted values. Instead, the code throws a TypeError. Please fix it so that the expected output is printed out.
+ 		    
 
 firstname  = input("Enter first name: ")
 secondname = input("Enter second name: ")
@@ -37,9 +37,7 @@ Expected output:
 9
 10
 
-# Question-031 : Create a function that calculates acceleration given initial velocity v1, final velocity v2, start time t1, and end time t2. 
-# The formula for acceleration is: a = (v2-v1) / (t2-t1)
-# call the function by inputting values 0, 10, 0, 20 for v1, v2, t1, and t2 respectively
+# Question-031 : Create a function that calculates acceleration given initial velocity v1, final velocity v2, start time t1, and end time t2. The formula for acceleration is: a = (v2-v1) / (t2-t1) call the function by inputting values 0, 10, 0, 20 for v1, v2, t1, and t2 respectively
 
 Expected output: 0.5
 
@@ -79,10 +77,8 @@ def foo():
 c = 3
 print(foo())
 
-# Question-036 : The following script throws a NameError in the last line saying that c is not defined. 
-#				 Please fix the function so that there is no error and the last line is able to 
-#				 print out the value of c  (i.e. 1 ).
-
+# Question-036 : The following script throws a NameError in the last line saying that c is not defined. Please fix the function so that there is no error and the last line is able to print out the value of c  (i.e. 1 ).
+				 
 def foo(): 
     c = 1 
     return c 
@@ -93,19 +89,33 @@ Expected output: 1
 
 # Question-037 : Create a function that takes any string as input and returns the number of words for that string.
 
-# Question-038 : Please download the words1.txt file from the attachment and then create a function that takes a text file 
-#   			 as input and returns the number of words contained in the text file ***
+# Question-038 : Please download the words1.txt file from the attachment and then create a function that takes a text file as input and returns the number of words contained in the text file ***			 
 
 Expected output : 10 
 
-# Question-039 : The following code is supposed to print out the square root of 9, 
-#				but it throws an error instead because another line before that is missing. 
-# 				Please fix the script so that it prints out the square root of 9.
-
+# Question-039 : The following code is supposed to print out the square root of 9, but it throws an error instead because another line before that is missing. Please fix the script so that it prints out the square root of 9.
+				
 math.sqrt(9) 
 
-# Question-040 : The script is supposed to output the cosine of angle 1 radian, but instead it is throwing an error. 
-#		    Please fix the code so that it prints out the expected output.
+# Question-040 : The script is supposed to output the cosine of angle 1 radian, but instead it is throwing an error. Please fix the code so that it prints out the expected output.    
 
 import math
 print(math.cosine(1))
+
+# Question-041 : Please try to guess what is missing in the following code and add the missing part so that the code works fine.
+
+import math
+print(math.pow(2))
+
+# Question-042 : Create a script that generates a text file with all letters of English alphabet inside it, one letter per line.
+
+# Question-043 : Print out in each line the sum of homologous items of the two sequences.
+
+a = [1, 2, 3]
+b = (4, 5, 6)
+
+Expected output: 
+
+5
+7
+9
