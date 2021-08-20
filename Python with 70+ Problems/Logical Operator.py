864b62fa466d326e5fd280cf40d09e675a02baae
mazed <mazedur@gmail.com>
2021-08-20 20:46:35 +0600

    Logical Operator

---
+++ Python with 70+ Problems/Logical Operator.py
@@ -0,0 +1,7 @@
+# Do the following Logical operations in computer 
+1. (3 is greater than 21) and (56 is equal to 23) 
+2. (4 is greater than or equal to 12) or (43 is lesser than or equal to 122) 
+3. (40 is lesser than 221) and (34 is not equal to 34) 
+4. (12 is lesser than or equal to 13) and (54 is greater than 23) 
+5. (26 is equal to 26) or (89 is not equal to 44) 
+6. (5 is not equal to 12) and (67 is lesser than 88)
