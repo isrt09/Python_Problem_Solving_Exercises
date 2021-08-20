12f942ae0a4bc665c983883877f3c1a9f8ee1f2b
mazed <mazedur@gmail.com>
2021-08-20 21:12:22 +0600

    Variables

---
+++ Python with 70+ Problems/Variables.py
@@ -0,0 +1,14 @@
+# Do the following creation of variables in computer 
+# Read the note for the conditions for the variable names ,otherwise it will through error
+
+1.Create a variable named var1 and assign multiplication of 2 and 3 to it to it 
+
+2.Create a variable named num and assign any odd number to it with decimals (float) 
+
+3.Create a variable named "name" and assign your name to it 
+
+4.Create a variable named "age" and assign your age to it (type int) 
+
+5.Create a variable named "item price" and assign a number to it (type str) 
+
+6.Create a variable named item2 and assign a value to it (type int)
