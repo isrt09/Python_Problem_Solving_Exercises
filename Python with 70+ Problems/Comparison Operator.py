f86f52b910677a96f6779ca1615290b199278533
mazed <mazedur@gmail.com>
2021-08-20 20:44:19 +0600

    Comparison Operator

---
+++ Python with 70+ Problems/Comparison Operator.py
@@ -0,0 +1,7 @@
+# Do the following comparison operations in computer
+1. 3  is greater than 21 
+2. 4  is greater than or equal to 12 
+3. 40 is lesser than 221 
+4. 12 is lesser than or equal to 13 
+5. 26 is equal to 26 
+6. 5  is not equal to 12
