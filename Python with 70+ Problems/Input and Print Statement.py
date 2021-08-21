59d66dbc3707be19cafa078657294ee09bf751df
mazed <mazedur@gmail.com>
2021-08-22 00:06:50 +0600

    Input and Print Statement

---
+++ Python with 70+ Problems/Input and Print Statement.py
@@ -0,0 +1,25 @@
+# Do this exercise in computer 
+
+# Get the input from the user and store it in variable named 'var'. 
+
+# Get the input from the user using hint 'Enter a number to print'
+
+1.Print the output of the variable
+
+2.Print the output of the variable along with the string stating "You have entered" using addition of strings.
+
+3.Print the output of the variable along with the string stating "You have entered" using % method
+
+4.Print the output of the variable along with the string stating "You have entered" using format method
+
+# Do multiplication of two numbers by getting the input from the user. Store the numbers in variable named 'num1' and 'num2' . 
+
+# Get the input from the user using hints
+
+1.Print the outputs of the variables in same line
+
+2.Print multiplication of the variables along with the user inputs using addition of strings.
+
+3.Print multiplication of the variables along with the user inputs using % method
+
+4.Print multiplication of the variables along with the user inputs using .format() method
