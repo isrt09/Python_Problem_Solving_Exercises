070aa8be34d5a680cd7b752f07668554c8fdc7a0
mazed <mazedur@gmail.com>
2021-08-22 21:42:18 +0600

    Decision Statements problems added

---
+++ Python with 70+ Problems/Decision statement.py
@@ -0,0 +1,59 @@
+# Decision Statement 
+# Do this exercise in computer 
+
+# Problem-1
+Write a program to get one 2 digit input from the user and display the relationship between the two numbers.
+
+Output statement must be exactly like below one just lesser , greater or equal to changes because it will perform automatic test case .
+
+For Example:
+Enter the number to find relationship : 23 
+
+The 1st digit of the number is lesser than 2nd digit of the number.
+
+# Problem-2
+Grade Converter
+Write a program which gets input from the user and converts it into grade depending upon the mark scored.
+
+Mark           Grade
+90 - 100         S
+80 - 90          A
+70 - 80          B
+60 - 70          C
+50 - 60          D
+40 - 50          E
+below 40         F
+
+Example:
+Enter the mark : 88
+You have scored A grade
+
+# Problem-3
+Palindrome.
+Palindrome is a word which gives same word if you write it reverse also.
+
+Example:
+MADAM 
+
+MADAM is a palindrome. 
+
+hello 
+
+hello is not a palindrome.
+
+# Problem-4
+Write a program which gets in put from the user and display whether the input consist of only letters or only numbers or both letters and numbers.
+
+Note : Read Notes if you need hint
+Example:
+Input : python 
+
+Output:The input contains only alphabets 
+
+Input : python3 
+
+Output : The input contains both digits and alphabets 
+
+Input : 1234 
+
+Output : The input contains only digits.
