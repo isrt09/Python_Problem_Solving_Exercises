09a32221ea76ddba95c26f32cfc8ef5f8dbd86ef
mazed <mazedur@gmail.com>
2021-08-22 14:18:13 +0600

    Tuples problems added

---
+++ Python with 70+ Problems/Tuple.py
@@ -0,0 +1,15 @@
+# Tuples (Do this exercise in computer or click here do it online)
+# Do the followings:
+# Note give any variable and give hint for print statements
+
+1.Create two tuples, 1st with values 12,3,4,5,5,7 and 2nd with 100
+
+2.Create another tuple assign it to concat of two tuples
+
+3.Display the max and min of the tuple.
+
+4.Do slicing in tuple and display them.
+
+5.Delete 3 from the tuple and display its length.
+
+6.Try to replace 12 from it.
