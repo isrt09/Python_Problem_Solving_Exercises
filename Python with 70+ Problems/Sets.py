0296011bc08169c8c5680afe8ae1920193faf85b
mazed <mazedur@gmail.com>
2021-08-22 14:27:54 +0600

    Sets problemes added

---
+++ Python with 70+ Problems/Sets.py
@@ -0,0 +1,16 @@
+# Set 
+# Do this exercise in computer
+# Do the following:
+# Note: Print with hint and do the following one by one
+
+1.Create a set.
+
+2.Remove the duplicates in the list.
+
+3.Try slicing and indexing in the set
+
+4.Convert list to tuples and tuples to sets and display them.
+
+5.Convert sets to tuples and tuples to list and display them .
+
+6.convert string to tuples and set and reverse them back to string.
