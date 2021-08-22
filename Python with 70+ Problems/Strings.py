1fe08df080952cbc4b04a21df439d49f4ba94622
mazed <mazedur@gmail.com>
2021-08-22 11:34:38 +0600

    Strings Manipulations

---
+++ Python with 70+ Problems/Strings.py
@@ -0,0 +1,26 @@
+Strings - Part 1
+# Do this exercise in computer 
+
+# Get a string value from the user and save it in variable 'string1' and write the code to get the following outputs:
+
+# Print the followings with hint like length of "hello world" is 11.
+1.Length of the string.
+
+2.First letter and last letter of the string.
+
+3.Upper case of the string.
+
+4.Lower case of the string.
+
+5.Slice the first two and last two words of the string.
+
+String - Part 2
+
+# Do this exercise in computer or click here do it online
+# Get two inputs from the user and store in two variables of any names. 
+# Do the following et the inputs separately which consist of escape characters and store in variables . 
+
+
+1.Print the input which consist of escape characters.(on 1st input)
+
+2.Print the input like you typed.(on 2nd input)
