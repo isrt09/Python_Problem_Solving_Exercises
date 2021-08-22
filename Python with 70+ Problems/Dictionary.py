675455db20b39428f980c3bc6eeeb883fe740ac8
mazed <mazedur@gmail.com>
2021-08-22 14:31:42 +0600

    Dictionary Problems added

---
+++ Python with 70+ Problems/Dictionary.py
@@ -0,0 +1,50 @@
+# Dictionary Part 1(Do this exercise in computer)
+# Do the following
+
+1.Create a dictionary which consist of Item(keys) and Quantity(values) of items in the shop.
+
+      Items            Quantity
+
+      soap                 10
+
+      bread                5
+
+      shampoo           8
+
+2.Create another dictionary which consist of Item(Keys) and Price(values) of the items in the shop
+
+      Items               Price
+
+       soap               20.50
+
+       bread             25.99
+
+       shampoo        80
+
+3.Display the item with quantity and the cost of item in a single line like
+
+The shop have 10 quantities of soap which cost 20.50 USD each.
+
+# Dictionary Part 2 (Do this exercise in computer)
+# do the following
+
+1.Display only the subjects that are in dict1.
+
+2.Display only the marks scored by student.
+
+3.Delete the subject in which he got low marks and display the dictionary.
+
+4.Change the mark scored in English to 24 and display the dictionary.
+
+5.Now try to get the value of the history if not available print proper message.1
+
+# Dictionary Part 3 (Do this exercise in computer)
+# do the following
+
+1. Add two dictionary and assign it to dict3 Display the dict3
+
+2. Clear the dictionary dict2 Display the dict2
+
+3. Delete the dictionary dict1.
+
+4.Get input from the user and print the score in that subject if not available print proper message. (user input can be of any case.)
