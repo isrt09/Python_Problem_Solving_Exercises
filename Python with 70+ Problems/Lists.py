6c027ee82c8dec39d5be049485d8dae4f1e8f264
mazed <mazedur@gmail.com>
2021-08-22 12:19:36 +0600

    Lists

---
+++ Python with 70+ Problems/Lists.py
@@ -0,0 +1,28 @@
+# List 
+# Do this exercise in computer 
+# Get a string from the user and store it in variable . Do the following.
+# Print them with hint and do one by one.
+
+1.Convert the string to list.
+
+2.Display the first element and last element of list.
+
+3.Display the elements except first and last elements in the list.
+
+4.Display the length of the list.
+
+5.Convert the list back to string and display them.
+
+6.Convert the string to list with respect to spaces i.e ['hello','world'] for input 'hello world'
+
+7.Add two lists and print them.
+
+8.Find the maximum and minimum of third list.
+
+9.Sort the third list and display them.
+
+10.Reverse the third list and display them.
+
+11.Remove the element '2' in the list and print them.
+
+12.Pop the last element from the list and print them
